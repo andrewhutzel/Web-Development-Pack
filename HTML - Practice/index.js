67a71f6871fcc6030b97c4594e32ee727afb4711
andrewhutzel <94021106+andrewhutzel@users.noreply.github.com>
2023-11-19 13:59:38 -0800

    Testing out an idea of utilizing a Calendar
    -Implemented a calendar tab
    -Reviewing fullcalendar.io for possible use cases.

---
+++ HTML - Practice/index.js
@@ -0,0 +1,14 @@
+const shoe = {
+    color: "Yellow",
+    size: 40,
+    price: 124.95,
+    bestSeller: true
+}
+
+var money =120;
+
+if(shoe.price > money){
+    console.log("You're too poor.")
+}else{
+    console.log("You're the right amount of poor.")
+}
