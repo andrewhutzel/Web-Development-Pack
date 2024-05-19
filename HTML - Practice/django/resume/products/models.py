3964bfad774f034e2829a249928413dd3bf640dd
andrewhutzel <94021106+andrewhutzel@users.noreply.github.com>
2024-05-18 22:22:49 -0700

    Applied Migrations
    -Finally applied migrations which changed:
    
     Applying contenttypes.0001_initial... OK
      Applying auth.0001_initial... OK
      Applying admin.0001_initial... OK
      Applying admin.0002_logentry_remove_auto_add... OK
      Applying admin.0003_logentry_add_action_flag_choices... OK
      Applying contenttypes.0002_remove_content_type_name... OK
      Applying auth.0002_alter_permission_name_max_length... OK
      Applying auth.0003_alter_user_email_max_length... OK
      Applying auth.0004_alter_user_username_opts... OK
      Applying auth.0005_alter_user_last_login_null... OK
      Applying auth.0006_require_contenttypes_0002... OK
      Applying auth.0007_alter_validators_add_error_messages... OK
      Applying auth.0008_alter_user_username_max_length... OK
      Applying auth.0009_alter_user_last_name_max_length... OK
      Applying auth.0010_alter_group_name_max_length... OK
      Applying auth.0011_update_proxy_permissions... OK
      Applying auth.0012_alter_user_first_name_max_length... OK
      Applying sessions.0001_initial... OK
    
    Pushing to django branch incase of needed revert or point of change.

--- HTML - Practice/django/resume/products/models.py
+++ HTML - Practice/django/resume/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+#Find all types of models here https://docs.djangoproject.com/en/5.0/ref/models/fields/
+
+class Shirt(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
